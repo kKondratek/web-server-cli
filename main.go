5016f9111a641c965f9ec2d2635ed9bb2f50a4df
kKondratek <k.kondratek@o2.pl>
2021-03-29 23:25:47 +0200

    Add run command

--- main.go
+++ main.go
@@ -3,18 +3,47 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
+
 	app := &cli.App{
-		Name:  "run",
-		Usage: "Run local http server",
-		Action: func(c *cli.Context) error {
-			fmt.Printf("Hello %q", c.Args().Get(0))
-			return nil
+		// TODO: main command
+		Commands: []*cli.Command{
+			{
+				Name:  "run",
+				Usage: "Run local http server",
+				Action: func(c *cli.Context) error {
+					var filepath string = c.String("file")
+					fmt.Printf("Filepath %q\n", filepath)
+
+					http.Handle("/", http.FileServer(http.Dir(filepath)))
+					http.ListenAndServe(":4000", nil)
+
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "file",
+						Aliases:  []string{"f"},
+						Usage:    "Specify index.html file",
+						Required: true,
+					},
+					// TODO: --port flag
+				},
+			},
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
 		},
 	}
 
@@ -22,4 +51,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getPath() {
+	// TODO: validate --file arg and return proper filepath
 }
